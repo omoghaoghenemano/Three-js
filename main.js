432ed329eff2196c3d1049a939865f2d78e9db95
omoghaoghenemano <manoomogha@yahoo.com>
2022-03-05 08:23:11 +0200

    Updates

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
-import './style.css';
+//import './style.css';
 
 //import * as THREE from 'three';
-import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls';
+//import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls';
 import * as THREE from 'https://unpkg.com/three@0.127.0/build/three.module.js'
 // Setup
 
