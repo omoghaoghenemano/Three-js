2ec187db326c5a06d4b50242bc1adcfccc11cf71
omoghaoghenemano <manoomogha@yahoo.com>
2022-03-05 09:17:15 +0200

    Updates

--- main.js
+++ main.js
@@ -8,7 +8,7 @@ const scene = new THREE.Scene();
 
 const camera = new THREE.PerspectiveCamera(60, window.innerWidth / window.innerHeight, 0.1, 1000);
 
-const renderer = new THREE.WebGL1Renderer({
+const renderer = new THREE.WebGLRenderer({
   canvas: document.querySelector('#bg'),
 });
 
