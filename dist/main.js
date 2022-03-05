8c120fe345438849ea180bb542c58085806273ae
omoghaoghenemano <manoomogha@yahoo.com>
2022-03-05 09:27:43 +0200

    new commit

--- dist/main.js
+++ dist/main.js
@@ -6,7 +6,7 @@ import * as THREE from 'https://unpkg.com/three/build/three.module.js';
 
 const scene = new THREE.Scene();
 
-const camera = new THREE.PerspectiveCamera(60, window.innerWidth / window.innerHeight, 0.1, 1000);
+const camera = new THREE.PerspectiveCamera(70, window.innerWidth / window.innerHeight, 0.1, 1000);
 
 const renderer = new THREE.WebGLRenderer({
   canvas: document.querySelector('.webgl'),
